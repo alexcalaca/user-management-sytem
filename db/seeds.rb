3fd5340cc3354604ad5b648f27c54352c8187cc5
alexcalaca <alexcalaca@gmail.com>
2022-08-17 04:24:38 -0300

    feat: include account status information to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ User.delete_all
 puts "###"
 2.times do |i|  
   user = User.create!(email: "email#{i+1}@email.com", password: "default")    
-  puts "User #{user.email} created. Role #{user.roles.pluck(:name)}"
+  puts "User #{user.email} created. Role: #{user.roles.pluck(:name)}. Account status: #{user.account_status}"
 end
 
 puts "###"
@@ -12,7 +12,7 @@ puts "###"
   user = User.create!(email: "email#{i+3}@email.com", password: "default")
   user.remove_role :newuser
   user.add_role :admin  
-  puts "User #{user.email} created. Role #{user.roles.pluck(:name)}"
+  puts "User #{user.email} created. Role: #{user.roles.pluck(:name)}. Account status: #{user.account_status}"
 end
 
 puts "###"
@@ -21,7 +21,7 @@ puts "###"
   user = User.create!(email: "email#{i+5}@email.com", password: "default")
   user.remove_role :newuser
   user.add_role :hr
-  puts "User #{user.email} created. Role #{user.roles.pluck(:name)}"
+  puts "User #{user.email} created. Role: #{user.roles.pluck(:name)}. Account status: #{user.account_status}"
 end
 
 puts "###"
