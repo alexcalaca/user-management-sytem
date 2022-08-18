5553b13e41c11415437a09269a113d92c896cc7f
alexcalaca <alexcalaca@gmail.com>
2022-08-18 15:58:56 -0300

    test: add dummy data to database
    seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,14 +2,14 @@ User.delete_all
 
 puts "###"
 2.times do |i|  
-  user = User.create!(email: "email#{i+1}@email.com", password: "default")    
-  puts "User #{user.email} created. Role: #{user.roles.pluck(:name)}. Account status: #{user.account_status}"
+  user = User.create!(email: "email#{i+1}@email.com", name: "name#{i+1}", password: "default")    
+  puts "User #{user.email} created.Role: #{user.roles.pluck(:name)}. Account status: #{user.account_status}"
 end
 
 puts "###"
 
 2.times do |i|  
-  user = User.create!(email: "email#{i+3}@email.com", password: "default")
+  user = User.create!(email: "email#{i+3}@email.com", name: "name#{i+3}", password: "default")
   user.remove_role :newuser
   user.add_role :admin  
   puts "User #{user.email} created. Role: #{user.roles.pluck(:name)}. Account status: #{user.account_status}"
@@ -18,7 +18,7 @@ end
 puts "###"
 
 2.times do |i|  
-  user = User.create!(email: "email#{i+5}@email.com", password: "default")
+  user = User.create!(email: "email#{i+5}@email.com", name: "name#{i+5}", password: "default")
   user.remove_role :newuser
   user.add_role :hr
   puts "User #{user.email} created. Role: #{user.roles.pluck(:name)}. Account status: #{user.account_status}"
@@ -27,7 +27,7 @@ end
 puts "###"
 
 2.times do |i|  
-  user = User.create!(email: "email#{i+7}@email.com", password: "default")    
+  user = User.create!(email: "email#{i+7}@email.com", name: "name#{i+7}", password: "default")    
   user.inactive!
   puts "User #{user.email} created. Role: #{user.roles.pluck(:name)}. Account status: #{user.account_status}"
 end
