71416daca808b1b47d094cfb4922df062aff1a78
alexcalaca <alexcalaca@gmail.com>
2022-08-18 23:35:52 -0300

    feat: set policy rules for users

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,8 +1,32 @@
 class UserPolicy < ApplicationPolicy
   class Scope < Scope
-    # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    def resolve
+      scope.all?
+    end
   end
+
+  def create?
+    @user.has_role? :hr
+  end
+
+  def index?    
+  end
+
+  def destroy?
+    @user.has_role? :admin
+  end
+
+  def delete?
+    @user.has_role? :admin
+  end
+
+  # Deactivate user
+  def edit? 
+    @user.has_any_role? :admin, :hr
+  end
+
+  # Deactivate user
+  def update?
+    @user.has_role? :admin
+  end    
 end
