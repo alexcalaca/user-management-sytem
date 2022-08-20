1c8145b0e999691a3dcd8e4fccb6e18010004030
alexcalaca <alexcalaca@gmail.com>
2022-08-19 23:14:43 -0300

    feat: include routes for searches controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users  
+  resources :users 
+  resources :searches
 
   devise_scope :user do  
     get '/users/sign_out' => 'devise/sessions#destroy'     
