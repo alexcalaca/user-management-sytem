cc464b28c2d2ef556ffb6c505ab5fc1bc105007b
alexcalaca <alexcalaca@gmail.com>
2022-08-19 23:16:34 -0300

    feat: create controller to handle the searches

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,2 +1,23 @@
 class SearchesController < ApplicationController
+  before_action :authenticate_user!
+  
+  def show
+    @search = Search.find(params[:id])
+  end
+
+  def new
+    @search = Search.new
+    @names = User.distinct.pluck(:name)
+    @emails = User.distinct.pluck(:email)
+    @roles = Role.distinct.pluck(:name)       
+  end
+
+  def create
+    @search = Search.create(search_params)
+    redirect_to @search
+  end
+
+  def search_params
+    params.require(:search).permit(:name, :email, :role)
+  end
 end
