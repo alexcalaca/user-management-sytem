d597b04fe3abc5940aa7376f464c7415780fb908
alexcalaca <alexcalaca@gmail.com>
2022-08-20 15:46:41 -0300

    feat: create search methods

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,2 +1,11 @@
 class Search < ApplicationRecord
+  self.inheritance_column = "not_sti"
+
+    def search_user
+        user = User.all
+        user = user.where(['name LIKE ?', name]) if name.present?
+        user = user.where(['email LIKE ?', email]) if email.present?
+        user = user.joins(:roles).where(['roles.name LIKE ?', role]) if role.present?
+        return user
+    end
 end
